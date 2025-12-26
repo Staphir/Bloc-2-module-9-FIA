0d4855ec150b1d61d8740af6ffaf434836b06ff4
Maxime Dulieu <m.dulieu@outlook.fr>
2025-12-26 15:24:06 +0100

    spring initializr

---
+++ src/main/java/com/iscod/FAI/FaiApplication.java
@@ -0,0 +1,13 @@
+package com.iscod.FAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iscod/FAI/FaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iscod.FAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
